300890900766c8c7ff6c588583998be508482db3
Le Văn Nhat <levannhat.23082003@gmail.com>
2025-05-27 20:29:49 +0700

    Bài 3 Ép Kiểu

---
+++ src/bai3_type_cast.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var a: Any="hello";
+    var str: String?=a as String
+
+
+    println(str)
+
+    var soa: Int=3
+    var sob: Long=soa.toLong()
+
+    println(sob)
+
+}
