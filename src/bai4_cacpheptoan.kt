7613694d209085c9436a5c612052a91415070f3a
Le Văn Nhat <levannhat.23082003@gmail.com>
2025-05-28 16:19:50 +0700

    Bài 3 Các Phép Toán

---
+++ src/bai4_cacpheptoan.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var a=3;
+    var b=6;
+//   var result= a.plus(b); cộng
+//   var result= a.minus(b); trừ
+//   var result= a.times(b); nhân
+//   var result= a.div(b); chia
+//   var result= a.rem(b); chia lấy dư
+    var result= a+b;
+    println(result);
+
+}
